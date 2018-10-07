948cb9a5b94bb8c32b335d91e52ddb013eed1c49
jyz <jyz@jyzdeiMac.local>
2018-10-07 20:01:58 +0800

    1231314

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by jyz on 2018/10/7.
+//  Copyright © 2018 jyz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
